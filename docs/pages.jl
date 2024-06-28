25b62206a08b37dc400c67df8cf914c608ad73a9
dynamic-queries <rahul.learns.physics@gmail.com>
2024-06-28 12:24:33 +0200

    update docs.

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,13 +1,12 @@
 pages = [
     "index.md",
+    "RKHS discretizations" => "rbf.md",
+    "Wavelet Discretizations" => "wavelets.md",
+    "Random Feature Discretizations" => "rfnn.md",
     "Tutorials" => [
         "tutorials/poisson.md",
         "tutorials/biharmonic.md",
         "tutorials/strum_liouville.md",
     ],
-    "Radial Bases Discretizations" => "rbf.md",
-    "Wavelet Discretizations" => "wavelets,md",
-    "Gamblet Discretizations" => "gamblets.md",
-    "Random Feature Discretizations" => "rfnn.md",
     "FAQs" => "faq.md"
 ]
