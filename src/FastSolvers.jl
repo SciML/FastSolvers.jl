7d5745af8b8775fb535eeccaaa63a1ea4b4ba50d
dynamic-queries <rahul.learns.physics@gmail.com>
2024-06-29 13:11:28 +0200

    Update interface for derivative operators.

--- src/FastSolvers.jl
+++ src/FastSolvers.jl
@@ -8,7 +8,13 @@ import QuasiMonteCarlo.SamplingAlgorithm
 import ModelingToolkit.PDESystem
 import ModelingToolkit.Equation
 
+include("problems.jl")
+export BVP, IVP
+export ∇, Δ, Δ²
+
 include("kernels.jl")
+export SquaredExponentialKernel, Matern52Kernel
+
 include("wavelets.jl")
 include("rfnn.jl")
 end
--- src/problems.jl
+++ src/problems.jl
@@ -1,31 +1,16 @@
 abstract type AbstractDifferentialOperator end
 
-# First order derivatives
-struct ∂₁ <: AbstractDifferentialOperator end
-struct ∂₂ <: AbstractDifferentialOperator end
-struct ∂₃ <: AbstractDifferentialOperator end
+# Abstract differential operators. See docs for detailed description.
+struct ∇ <: AbstractDifferentialOperator end
+struct Δ <: AbstractDifferentialOperator end
+struct Δ² <: AbstractDifferentialOperator end
 
-# Second order derivatives
-struct ∂₁₁ <: AbstractDifferentialOperator end
-struct ∂₁₂ <: AbstractDifferentialOperator end
-struct ∂₁₃ <: AbstractDifferentialOperator end
-struct ∂₂₂ <: AbstractDifferentialOperator end
-struct ∂₂₃ <: AbstractDifferentialOperator end
-struct ∂₃₃ <: AbstractDifferentialOperator end
+abstract type AbstractFastSolver end
 
-# Some fourth order derivatives
-struct ∂₁₁₁₁ <: AbstractDifferentialOperator end
-struct ∂₂₂₂₂ <: AbstractDifferentialOperator end
-struct ∂₃₃₃₃ <: AbstractDifferentialOperator end
-struct ∂₄₄₄₄ <: AbstractDifferentialOperator end
-struct ∂₁₁₂₂ <: AbstractDifferentialOperator end
-struct ∂₂₂₃₃ <: AbstractDifferentialOperator end
-struct ∂₁₁₃₃ <: AbstractDifferentialOperator end
+struct BVPSolver <: AbstractFastSolver
 
-abstract type AbstractPDEProblem end
-
-struct BVPProblem <: AbstractPDEProblem
 end
 
-struct IVPProblem <: AbstractPDEProblem
+struct IVPSolver <: AbstractFastSolver
+
 end
