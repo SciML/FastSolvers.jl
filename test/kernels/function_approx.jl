fbf4a1a2cb6881a661911f4787725c312c1949a2
dynamic-queries <rahul.learns.physics@gmail.com>
2024-07-27 19:24:00 +0200

    add function approximation derivative test.

--- test/kernels/function_approx.jl
+++ test/kernels/function_approx.jl
@@ -1,4 +1,3 @@
-# Test one instance of the approximator
 begin
     N = 1000
     N0 = 100
@@ -22,11 +21,47 @@ begin
 end
 
 
-N = 1000
-N0 = 100
-x = LinRange(-2.0, 2.0, N)
-y = sin.(14 * x) .* cos.(15 * x)
-kernel = SquaredExponentialKernel(1e-1)
-Ops = [∇(), Δ()]
-kernel_eval = z -> kernel(z, y)
-kernel_deriv = z -> kernel(z, y, Ops[1])
+begin
+    N = 1000
+    N0 = 100
+    x = LinRange(-2.0, 2.0, N)
+    y = sin.(15 * x) .* cos.(15 * x)
+    fy = 0.5*sin.(30*x)
+    ffy = 15.0*cos.(30*x)
+
+    kernel = SquaredExponentialKernel(1e-1)
+    xcolloc = QuasiMonteCarlo.sample(N0, x[1], x[end], LatinHypercubeSample())
+    Ops = [∇(), Δ()]
+    Dkernel = (x,y) -> kernel(x,y,Ops[1])
+    D2kernel = (x,y) -> kernel(x,y,Ops[1])
+
+    K = spzeros(N, N0)
+    for i=1:N
+        for j=1:N0
+            K[i,j] = Dkernel(x[i], xcolloc[j])
+        end
+    end
+    f = (a, p) -> K * a - fy
+    a0 = zeros(N0)
+    prob = NonlinearLeastSquaresProblem(f, a0)
+    sol = solve(prob)
+    a = sol.u
+    @test maximum(K*a .- fy) < 1e-2
+
+
+    N0 = 150
+    xcolloc = QuasiMonteCarlo.sample(N0, x[1], x[end], LatinHypercubeSample())
+    K = spzeros(N, N0)
+    for i=1:N
+        for j=1:N0
+            K[i,j] = D2kernel(x[i], xcolloc[j])
+        end
+    end
+    f = (a, p) -> K * a - ffy
+    a0 = zeros(N0)
+    prob = NonlinearLeastSquaresProblem(f, a0)
+    sol = solve(prob)
+    a = sol.u
+
+    @test maximum(K*a .- ffy) < 1e-2
+end
